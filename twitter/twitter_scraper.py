6fe339497e08e5ff84438b70d5b2a31ca7cad272
zzoroak <zzoroak@gmail.com>
2026-04-20 09:56:30 +0900

    Filter tweets using gemini

--- twitter/twitter_scraper.py
+++ twitter/twitter_scraper.py
@@ -1,6 +1,9 @@
 import requests
 from datetime import datetime, timedelta, timezone
 from config import BEARER_TOKEN
+from config import GOOGLE_AI_API_KEY
+from prompts import twitter_prompt
+
 
 USERNAME = "Jaemyung_Lee"
 USER_ID = "106379129"
@@ -8,14 +11,13 @@ USER_ID = "106379129"
 
 def get_recent_tweets():
     now = datetime.now(timezone.utc)
-    since = now - timedelta(days=1)
+    since = now - timedelta(days=5) # 추출 시간
 
     start_time = since.strftime("%Y-%m-%dT%H:%M:%SZ")
 
     url = f"https://api.x.com/2/users/{USER_ID}/tweets"
 
     params = {
-        "max_results": 10, # 과금 제한
         "start_time": start_time,
         "tweet.fields": "created_at,text,note_tweet"
     }
@@ -32,19 +34,46 @@ def get_recent_tweets():
 
     return res.json().get("data", [])
 
+def is_related_to_sk_gas(text):
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={GOOGLE_AI_API_KEY}"
+
+    payload = {
+        "contents": [
+            {
+                "parts": [{"text": twitter_prompt(text)}]
+            }
+        ]
+    }
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    res = requests.post(url, headers=headers, json=payload)
+
+    if res.status_code != 200:
+        print("Gemini error:", res.text)
+        return False
+
+    result = res.json()
+    answer = result["candidates"][0]["content"]["parts"][0]["text"].strip()
+
+    return answer.upper().startswith("YES")
 
 def main():
     tweets = get_recent_tweets()
 
     for t in tweets:
-        print("=" * 50)
-        print("날짜:", t["created_at"])
-
         if "note_tweet" in t and "text" in t["note_tweet"]:
             full_text = t["note_tweet"]["text"]
         else:
             full_text = t["text"]
 
+        if not is_related_to_sk_gas(full_text):
+            continue
+
+        print("=" * 50)
+        print("날짜:", t["created_at"])
         print("내용:", full_text)
         print("URL:", f"https://x.com/{USERNAME}/status/{t['id']}")
 
